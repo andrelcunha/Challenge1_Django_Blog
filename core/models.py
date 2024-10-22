8e8aa788d68280601025cd14ec33a1e40e015cf9
Andre Luis da Cunha <andreluiscunha81@gmail.com>
2024-10-22 17:14:05 -0300

    add models "post"," tag"

--- core/models.py
+++ core/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Titulo")
+    content = models.TextField(verbose_name="Conteúdo")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Criado em")
+    tags = models.ManyToManyField('Tag')
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = "Post"
+        verbose_name_plural = "Posts"
+        ordering = ['-created_at']
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Nome")
+
+    def __str__(self):
+        return self.name
+    class Meta:
+        verbose_name = "Tag"
+        verbose_name_plural = "Tags"
+        ordering = ['name']
